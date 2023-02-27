194808b160828cc60be662d6964b92294d75a189
Wisam Abdo <wisamabdo@hotmail.com>
2023-02-27 22:12:36 +0000

    Completion of playlist.adb to incorperate variants

--- lab4-records/ada/playlist.adb
+++ lab4-records/ada/playlist.adb
@@ -14,37 +14,53 @@ procedure PlayList is
         Put (To_String (p.name));
     end Put_Person;
 
-    type Item is record
-        name        : Unbounded_String;
-        performer   : Person;
+    type item_variant is (Pause, Piece);
+
+    type Item(variant : item_variant) is record
         length_secs : Float;
+        case variant is
+            when Pause =>
+                null;
+            when Piece =>
+                name        : Unbounded_String;
+                performer   : Person;
+        end case;
     end record;
 
     procedure Put_Item (i : Item) is
     begin
-        Put (To_String (i.name));
-        Put (" by ");
-        Put_Person (i.performer);
-        Put (" (");
-        Put (i.length_secs, aft => 1, exp => 0);
-        Put ("s)");
+        case i.variant is
+            when Pause =>
+                Put ("(Pause ");
+                Put (i.length_secs, aft => 1, exp => 0);
+                Put ("s)");
+            when Piece =>
+                Put (To_String (i.name));
+                Put (" by ");
+                Put_Person (i.performer);
+                Put (" (");
+                Put (i.length_secs, aft => 1, exp => 0);
+                Put ("s)");
+            end case;
     end Put_Item;
 
     piece1 : Item :=
-       (name => To_Unbounded_String ("Moonlight Sonata"),
+       (
+        variant => Piece,
+        name => To_Unbounded_String ("Moonlight Sonata"),
         performer => (name => To_Unbounded_String ("Claudio Arrau")),
         length_secs => 17.0*60.0+26.0
        );
 
-    -- pause1 : Item :=
-    --    (
-    --     item_variant => PAUSE,
-    --     length_secs => 5.0
-    --    );
+    pause1 : Item :=
+      (
+       variant => Pause,
+       length_secs => 5.0
+      );
 
 begin
     Put_Item (piece1);
     Put_Line ("");
-    -- Put_Item(pause1);
-    -- Put_Line ("");
+    Put_Item(pause1);
+    Put_Line ("");
 end PlayList;
